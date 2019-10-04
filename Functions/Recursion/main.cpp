700caafadb6d297bf8663acc1d95e2f3e9447609
Student <Student@304-1>
2019-10-04 20:55:23 +0300

    havent done

--- Functions/Recursion/main.cpp
+++ Functions/Recursion/main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+
 void print(int n)
 {
 	if (n == 0)return;
@@ -8,9 +9,34 @@ void print(int n)
 	
 }
 
+double power(int a, int n)
+{
+	return n == 0 ? 1 : n > 0 ? a * power(a, n - 1) : 1 / (a*power(a, n + 1));
+}
+
+void fibonacci(int double)
+{
+	int a = 0;
+	int b = 1;
+	cout << a << " " << b << " ";
+	for (i = -1; i < n/4; i++)
+	{
+		a += b; cout << a << " ";
+		b += a; cout << b << " ";
+	}
+	
+}
+
 void main()
 {
 	setlocale(LC_ALL, "");
-	print(10);
+	double a, n;
+	
+	/*cout << "¬ведите основание степени: "; cin >> a; cout << endl;
+	cout << "¬ведите показатель степени: "; cin >> n; cout << endl;
+	cout << a << " ^ " << n << " = "<< power(a, n) << endl;*/
+	cout << "—колько чисел ‘ибоначи вывести на экран: "; cin >> n; cout << endl;
+	fibonacci(n);
+
 	
 }
