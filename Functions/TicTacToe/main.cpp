088cd9c98fa621c0e944a2068c50e7fec2747e38
Student <Student@304-1>
2019-10-11 21:11:18 +0300

    X/0

--- Functions/TicTacToe/main.cpp
+++ Functions/TicTacToe/main.cpp
@@ -6,16 +6,20 @@ using namespace std;
 
 void PrintField(char Field[], const int n);
 
+void Move(char Field[], const int n);
+
 void main()
 {
 	setlocale(LC_ALL, "Rus");
 	const int SIZE = 9+1;
-	char Field[SIZE] = "123456789";	//Игровое поле
+	char Field[SIZE] = " ";	//Игровое поле
 	PrintField(Field, SIZE);
 }
 
 void PrintField(char Field[], const int n)
 {
+	
+	system("CLS");
 	cout << "\n\n\n\n\n";
 	for (int i = 6; i >= 0; i -= 3)
 	{
@@ -28,4 +32,16 @@ void PrintField(char Field[], const int n)
 		}cout << endl; HORIZ_OFFSET
 		if(i!=0) cout << "---+---+---"<< endl;
 	}cout << "\n\n\n";
+	Move(Field, n);
+}
+
+void Move(char Field[], const int n)
+{
+	char key;
+	key = _getch();
+	if (key == 27)return;
+	if (key == 27) 
+	/*cout << (int)key << endl;*/
+	Field[key - 49] = key;
+	PrintField(Field, n);
 }
