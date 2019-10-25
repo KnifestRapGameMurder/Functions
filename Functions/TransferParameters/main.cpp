c66b7e3502010f04e117ce2af5dddf7d8d7c0dc9
Student <Student@304-1>
2019-10-25 20:53:58 +0300

    transfer

--- Functions/TransferParameters/main.cpp
+++ Functions/TransferParameters/main.cpp
@@ -1,19 +1,19 @@
 #include<iostream>
 using namespace std;
 
-void Exchange(int a, int b);
+void Exchange(int* a, int* b);
 
 void main()
 {
 	int a = 2;
 	int b = 3;
 	cout << a << "\t" << b << endl;
-	Exchange(a, b);
-}
-void Exchange(int a, int b)
-{
-	int buffer = a;
-	a = b;
-	b = buffer;
+	Exchange(&a, &b);
 	cout << a << "\t" << b << endl;
+}
+void Exchange(int* a, int* b)
+{
+	int buffer = *a;
+	*a = *b;
+	*b = buffer;
 }
