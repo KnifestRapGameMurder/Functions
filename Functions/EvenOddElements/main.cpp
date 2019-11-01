4371f3b125b3fc91c78fc2bc9cf417bad8a99ffc
Student <Student@304-1>
2019-11-01 20:16:06 +0200

    evenodd

--- Functions/EvenOddElements/main.cpp
+++ Functions/EvenOddElements/main.cpp
@@ -8,24 +8,11 @@ void main()
 	int Arr[n];
 	int even_n = 0;
 	int odd_n = 0;
-	for (int i = 0; i < n; i++)
-	{
-		Arr[i] = rand() % 100;
-		cout << Arr[i] << "\t";
-		Arr[i] % 2 == 0 ? even_n++ : odd_n++;
-	}cout << endl;
+	for (int i = 0; i < n; i++)	cout << (Arr[i] = rand() % 100) << "\t";	cout << endl;
+	for (int i = 0; i < n; i++)	Arr[i] % 2 ? odd_n++ : even_n++;
 	int *Even_value = new int[even_n];
 	int *Odd_value = new int[odd_n];
-	for (int i = 0, j=0,k=0; i < n; i++)
-	{
-		Arr[i] % 2 == 0 ? Even_value[j++] = Arr[i] : Odd_value[k++] = Arr[i];
-	}
-	for (int i = 0; i < even_n; i++)
-	{
-		cout << Even_value[i] << "\t";
-	}cout << endl;
-	for (int i = 0; i < odd_n; i++)
-	{
-		cout << Odd_value[i] << "\t";
-	}cout << endl;
+	for (int i = 0, j=0,k=0; i < n; i++)Arr[i] % 2 == 0 ? Even_value[j++] = Arr[i] : Odd_value[k++] = Arr[i];
+	for (int i = 0; i < even_n; i++)cout << Even_value[i] << "\t";	cout << endl;
+	for (int i = 0; i < odd_n; i++)	cout << Odd_value[i] << "\t";	cout << endl;
 }
