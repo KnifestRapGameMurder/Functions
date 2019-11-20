5c4244df11a99cb88759077b6928ad549b12aeb6
Young Lover <younglover17771@gmail.com>
2019-11-20 17:31:06 +0200

    dz str

--- NullTerminatedLines/NullTerminatedLines/main.cpp
+++ NullTerminatedLines/NullTerminatedLines/main.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 void inputLine(char str[], const int n);
 int StrLen(char str[]);
-char UpperCase(int& str, const int n);
-char LowerCase(int& str, const int n);
-char Capitalize(int& str, const int n);
-char Shrink(int& str, const int n);
-char RemoveSpaces(int& str, const int n);
+void UpperCase(char str[], const int n);
+void LowerCase(char str[], const int n);
+void Capitalize(char str[], const int n);
+void Shrink(char str[], const int n);
+void RemoveSpaces(char str[], const int n);
+void isPalindrome(char str[], const int n);
 
 
 void main()
@@ -19,12 +20,23 @@ void main()
 	cout << sizeof("Hello") << endl;
 	cout << sizeof(str) << endl;
 	cout << str << endl;*/
-	const int n = 20;
+	const int n = 256;
 	char str[n];
 	cout << "¬ведите строку: "; 
 	inputLine(str, n);
 	cout << str << endl;
 	cout << StrLen(str) << endl;
+	isPalindrome(str, n);
+	Capitalize(str, n);
+	cout << str << endl;
+	UpperCase(str, n);
+	cout << str << endl;
+	LowerCase(str, n);
+	cout << str << endl;
+	Shrink(str, n);
+	cout << str << endl;
+	RemoveSpaces(str, n);
+	cout << str << endl;
 }
 
 void inputLine(char str[], const int n)
@@ -40,30 +52,35 @@ int StrLen(char str[])
 	for (; str[i]; i++);
 	return i;
 }
-char UpperCase(int& str, const int n)
+void UpperCase(char str[], const int n)
 {
-	for (int i = 0; i < n; i++)
+	for (int i = 0; str[i]; i++)	if (((int)str[i] > 96 && (int)str[i] < 123) || ((int)str[i] > 223 && (int)str[i] < 256))	str[i] = (int)str[i] - 32;
+}
+void LowerCase(char str[], const int n)
+{
+	for (int i = 0; str[i]; i++)	if (((int)str[i] > 64 && (int)str[i] < 91) || ((int)str[i] > 191 && (int)str[i] < 224))	str[i] = (int)str[i] + 32;
+}
+void Capitalize(char str[], const int n)
+{
+	for (int i = 0; str[i]; i++)	if (((int)str[i] > 96 && (int)str[i] < 123 || (int)str[i] > 223 && (int)str[i] < 256) && ((int)str[i-1]==32|| i==0))	str[i] = (int)str[i] - 32;
+}
+void Shrink(char str[], const int n)
+{
+	for (int i = 0; str[i]; i++)	if ((int)str[i] == 32)	while((int)str[i + 1] == 32) { for (int j = i; str[j]; j++) str[j] = str[j+1];};
+}
+void RemoveSpaces(char str[], const int n)
+{
+	for (int i = 0; str[i]; i++)	if ((int)str[i] == 32)	for (int j = i; str[j]; j++) str[j] = str[j + 1];
+}
+void isPalindrome(char str[], const int n)
+{
+	char t = 1;
+	for (int i = 0; i < StrLen(str)-i-1; i++)
 	{
-		if (str[i] > 96 && str[i] < 123)
+		if (str[i] != str[StrLen(str) - i - 1])
 		{
-
+			cout << "Isn't palidrome" << endl; t = 0; return;
 		}
 	}
-	return str;
-}
-char LowerCase(int& str, const int n)
-{
-	return str;
-}
-char Capitalize(int& str, const int n)
-{
-	return str;
-}
-char Shrink(int& str, const int n)
-{
-	return str;
-}
-char RemoveSpaces(int& str, const int n)
-{
-	return str;
+	if(t) 	cout << "Is palidrome" << endl;
 }
