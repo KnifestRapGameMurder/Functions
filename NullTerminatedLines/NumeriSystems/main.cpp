abf9d9aca0c24de3ada0f00441fcb8272194d3af
Student <Student@304-1>
2019-12-04 20:50:11 +0200

    str

--- NullTerminatedLines/NumeriSystems/main.cpp
+++ NullTerminatedLines/NumeriSystems/main.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
 #include<Windows.h>
-using namespace std;
 
 char* DecToBin(int dec);
-int BinToDec(char* binary, int n);
-void isBin(char* binary, int n);
+int BinToDec(char* str, int n);
+bool isBin(char* str, int n);
+int HexToDec(char* str, int n);
+bool isHex(char* str, int n);
 
 void main()
 {
@@ -13,50 +14,95 @@ void main()
 	char* binary = DecToBin(dec);
 	cout << binary << endl;
 	delete[] binary;*/
-	int n = 32;
-	cout << "¬ведите двоичное число: "; char* binary = new char[n]; cin.getline(binary, n);
-	n = strlen(binary);
-	cout << BinToDec(binary,n) << endl;
-	delete[] binary;
+	/*int n = 32;
+	cout << "¬ведите двоичное число: "; char* str = new char[n]; cin.getline(str, n);
+	n = strlen(str);*/
+	int n = 11;
+	std::cout << "¬ведите Hex число: "; char* str = new char[n]; std::cin.getline(str, n);
+	n = strlen(str);
+	if(isBin(str, n))	std::cout <<"Bin:"<< BinToDec(str,n) << std::endl;
+	if (isHex(str, n)) std::cout << "Hex:" << HexToDec(str,n) << std::endl;
+	delete[] str;
 
 }
 
 char* DecToBin(int dec)
 {
 	int n = 32;
-	char* binary = new char[n] {};
-	for (int i = 0; dec; i++, dec /= 2)binary[i] = dec % 2 + 48;
+	char* str = new char[n] {};
+	for (int i = 0; dec; i++, dec /= 2)str[i] = dec % 2 + 48;
 	/*for (n--; n >= 0; n--)
 	{
 		cout << binary[n];
 		n % 8 == 0 ? cout << "  " : n % 4 == 0 ? cout << " " : cout << "";
 	}
 	cout << endl;*/
-	n = strlen(binary);
-	for (int i = 0; i< n / 2; i++)	swap(binary[i], binary[n - 1 - i]);
-	return binary;
+	n = strlen(str);
+	for (int i = 0; i< n / 2; i++)	std::swap(str[i], str[n - 1 - i]);
+	return str;
 }
 
-int BinToDec(char* binary,int n)
+int BinToDec(char* str,int n)
 {
 	int dec;
 	if (n == 1) return 1;
-	dec = (binary[0] - 48) * 2 + binary[1] - 48;
+	dec = (str[0] - 48) * 2 + str[1] - 48;
 	for (int i = 2; i < n; i++)
 	{
-		dec = (dec * 2) + binary[i]-48;
+		dec = (dec * 2) + str[i]-48;
 	}
 	return dec;
 }
 
-void isBin(char* binary, int n)
+bool isBin(char* str, int n)
 {
 	for (int i = 0; i < n; i++)
 	{
-		if (binary[i] - 48 != 0 && binary[i] - 48 != 1)
+		if (str[i] - 48 != 0 && str[i] - 48 != 1)
 		{
-			cout << "Not binary!!!" << endl;
-			return;
+			return false;
 		}
 	}
+	return true;
+}
+
+int HexToDec(char* str, int n)
+{
+	int dec = 0;
+	for (int i = 0; i < n; i++)
+	{
+		dec *= 16;
+		if (str[i] >= '0'&&str[i] <= '9')dec += str[i] - '0';
+		else if (str[i] >= 'A'&&str[i] <= 'F')dec += str[i] - 'A'+10;
+		else if (str[i] >= 'a'&&str[i] <= 'f')dec += str[i] - 'a' + 10;
+		else throw 0;
+	}
+	return dec;
+}
+
+bool isHex(char* str, int n)
+{
+	int i = 0;
+	if (str[0] == '0' && (str[1] == 'x' || str[1] == 'X'))
+	{
+		for (i = 0; i < n - 2; i++)
+		{
+			str[i] = str[i + 2];
+			str[n - 1] = 'S'; str[n - 2] = 'S';
+		}
+	}
+	if (str[2] == 0) return false;
+	for (i=0; i < n; i++)
+	{
+		if (
+			!(str[i]>='0'&&str[i]<='9')&&
+			!(str[i]>='a'&&str[i]<='f')&&
+			!(str[i]>='A'&&str[i]<='F')
+			)
+		{
+			return false;
+		}
+	}
+	
+	return true;
 }
