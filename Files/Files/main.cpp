b82858dca193c674a8a52c22f26ee41b398f1cd9
Student <Student@304-1>
2019-12-11 21:04:49 +0200

    size of files

--- Files/Files/main.cpp
+++ Files/Files/main.cpp
@@ -12,46 +12,37 @@
 void main()
 {
 	setlocale(LC_ALL, "");
-
-	int p = 0;
-	bool txt = 0;
+	//							
 	std::ifstream fin;
 	const int t = 256;
 	char src_f_name[t] = {};
-	std::cout << "Введите имя файла: ";
+	std::cout << "Введите имя исхожного файла: ";
 	std::cin.getline(src_f_name, t);
-	while (p < t||!txt)
-	{
-		p++;
-		if (src_f_name[p] == '.'&&src_f_name[p + 1] == 't'&&src_f_name[p + 2] == 'x'&&src_f_name[p + 3] == 't') txt = 1;
-	};
-	p = 0;
-	while (p < t&&!txt)
-	{
-		p++;
-		if (src_f_name[p] == 0)
-		{
-			src_f_name[p] = '.';
-			src_f_name[p + 1] = 't';
-			src_f_name[p + 2] = 'x';
-			src_f_name[p + 3] = 't';
-		}
-		txt = 1;
-	};
+	if (!strstr(src_f_name, ".txt"))	strcat_s(src_f_name, ".txt");
 	std::cout << std::endl;
 	fin.open(src_f_name);
 
+	std::streampos begin, end;
+	std::ifstream src_file(src_f_name, std::ios::binary);
+	begin = src_file.tellg();
+	src_file.seekg(0, std::ios::end);
+	end = src_file.tellg();
+	src_file.close();
+	std::cout << "\n\t\t{size is: " << (end - begin) << " bytes.}\n\n";
+	
 	const int n = 256;
 	char sz_buffer[n] = {};
+	std::ofstream fout;
 
-	std::ofstream fout;	
 	const int e = 256;
 	char fnl_f_name[t] = {};
 	std::cout << "Введите имя файла: ";
-	std::cin.getline(fnl_f_name, t);	std::cout << std::endl;
+	std::cin.getline(fnl_f_name, t);
+	if (!strstr(fnl_f_name, ".txt"))	strcat_s(fnl_f_name, ".txt");	std::cout << std::endl;
 	fout.open(fnl_f_name, std::ios::app);
-	p = 1;
-	while (!fin.eof())
+
+	int p = 1;
+	while (!fin.eof()&&fin.is_open())
 	{
 		fin.getline(sz_buffer, n);
 		int i = 0;
@@ -64,10 +55,17 @@ void main()
 		do { fout << sz_buffer[i]; i++; } while (sz_buffer[i] != 0);
 		fout << ";\n}\n\n";
 	}
-	
+
 	fin.close();
 	fout.close();
 
+	std::ifstream fnl_file(fnl_f_name, std::ios::binary);
+	begin = fnl_file.tellg();
+	fnl_file.seekg(0, std::ios::end);
+	end = fnl_file.tellg();
+	fnl_file.close();
+	std::cout << "\n\t\t{size is: " << (end - begin) << " bytes.}\n\n";
+	
 	char command[n] = "notepad ";
 	strcat_s(command, fnl_f_name);
 	system(command);
